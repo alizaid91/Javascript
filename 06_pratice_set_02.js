11544524b6f4aeb61cb66041e4ecbb7ec1768978
Ali Zaid <alizaidshaikh9975@gmail.com>
2024-01-30 20:32:39 +0530

    lecture 8 completed

--- 06_pratice_set_02.js
+++ 06_pratice_set_02.js
@@ -10,6 +10,26 @@ if (age > 10 && age < 20) {
   console.log("No, Age is not in between 10 and 20");
 }
 
+//Q2. switch case statement
+let number = prompt("Enter number: ");
+switch (number) {
+  case "12":
+    console.log("You number is 12");
+    break;
+  case "13":
+    console.log("Your number is 13");
+    break;
+  case "14":
+    console.log("Your number is 14");
+    break;
+  case "15":
+    console.log("Your number is 15");
+    break;
+  default:
+    console.log("Your number is not spacial");
+    break;
+}
+
 //Q3,Q4. program to find whether the give number is divisible by 2 and 3
 let num = prompt("Enter a number: ");
 
@@ -23,13 +43,7 @@ if (num % 2 == 0 && num % 3 == 0) {
   console.log("Number is nither divisible by 2 nor 3! ");
 }
 
-//Q5. deiving code
+//Q5. driving code using ternary oprator
 let age2 = prompt("Enter your age: ");
-
-if (age2 < 0) {
-  console.log("Invalid age!");
-} else if (age2 < 18) {
-  console.log("You can not drive!");
-} else {
-  console.log("You can drive!");
-}
+let a = age2 > 18 ? "You can drive" : "You cannot drive";
+console.log(a);
